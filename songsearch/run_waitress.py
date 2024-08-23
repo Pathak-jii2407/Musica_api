2a5398fb85ecf71ea53e4fe86c4b94b07003e4c9
ved prakash pathak <156571640+Pathak-jii2407@users.noreply.github.com>
2024-08-24 03:50:12 +0530

    Update run_waitress.py

--- songsearch/run_waitress.py
+++ songsearch/run_waitress.py
@@ -3,6 +3,6 @@ from waitress import serve
 from songsearch.wsgi import application
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NTI.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'songsearch.settings')
     serve(application, host='0.0.0.0', port=8000)
 
